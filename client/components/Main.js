2e13d10a514cad3994ee0202649031ad60a0f566
Kenny Koch <kennykoch47@gmail.com>
2018-11-08 23:46:26 -0500

    adding react

---
+++ client/components/Main.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import axios from axios;
+
+const Home = () => (
+  <div>
+    <h1>if you see this it worked</h1>
+  </div>
+);
+
+export default Home;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // Using express: http://expressjs.com/
 const express = require('express');
+const path = require('path')
 // Create the app
 const app = express();
 
 const PORT = process.env.PORT || 9000;
+
+// app.use(express.static(path.join(__dirname, './public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${  PORT}`);
