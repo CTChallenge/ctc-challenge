f0ea67e6e5582382fe9884590607761c41f72817
Kenny Koch <kennykoch47@gmail.com>
2018-11-09 12:58:27 -0500

    added chart.js

--- client/components/data.js
+++ client/components/data.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Chart from 'chart.js'
+import Chart from 'chart.js';
+import axios from 'axios';
 
 
 export default class Data extends Component {
@@ -11,21 +12,27 @@ export default class Data extends Component {
   }
 
   componentDidMount() {
-    let ctx = document.getElementById("myChart").getContext('2d');
-    let myChart = new Chart(ctx, {
+    axios.get('https://data.ct.gov/resource/deaths.json')
+      .then((res) => {
+        console.log(res.data);
+      });
+    const ctx = document.getElementById("myChart").getContext('2d');
+    const myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Hartford", "New Haven", "Middlesex", "Litchfield", "New London", "Tolland", "Windham"],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: '# of Overdose Deaths',
+          data: [12, 19, 3, 5, 2, 3, 4, 12],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
+            'rgba(255, 159, 64, 0.7)',
+            'rgba(41, 107, 172, 0.7)',
+            'rgba(62, 154, 39, 0.7)',
           ],
           borderColor: [
             'rgba(255,99,132,1)',
@@ -55,7 +62,7 @@ export default class Data extends Component {
     return (
       <div>
         <div>data goes here</div>
-        <canvas id="myChart" width="400" height="400" />
+        <canvas id="myChart" width="200" height="200"></canvas>
       </div>
     );
   }
