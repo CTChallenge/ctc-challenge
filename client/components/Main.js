5d23e60d4fd7b039d1b9f9edaba4f4ba78676c88
Kenny Koch <kennykoch47@gmail.com>
2018-11-09 11:40:53 -0500

    added data component

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Data from './data'
 // import axios from axios;
 
 const Home = () => (
   <div>
     <h1>if you see this it worked</h1>
+    <Data />
   </div>
 );
 
