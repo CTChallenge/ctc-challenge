759d63044517306b7d2e55f4c74e9cde6bbc1377
Jason A. Young <youngj25@southernct.edu>
2018-11-13 17:57:15 -0500

    Add files via upload

--- client/components/mapContainer.js
+++ client/components/mapContainer.js
@@ -57,8 +57,10 @@ export class MapContainer extends Component {
      onMarkerClick(props, marker, e) {
 		 console.log(props);
 		 document.getElementById("markerTitle").innerHTML = props.title;
-		 document.getElementById("markerAddress").innerHTML = props.address;
+		 var htmlText = "<b>Address:</b> <br/> "+props.address +"<br/>"+ props.city + ", CT "+ props.zip.substring(0,5);
+		 document.getElementById("markerAddress").innerHTML = htmlText;
 		 //document.getElementById("markerAddress").innerHTML = <b>Address:</b> <br> props.address <br> props.city + ", CT "+ props.zip.substring(0,5);
+		 htmlText =
 		 document.getElementById("markerPhone").innerHTML =  props.phone;
 		 //document.getElementById("markerPhone").innerHTML = <b>"Phone:"+</b> <a href="tel:"+props.phone>  props.phone </a> ;
 		 
