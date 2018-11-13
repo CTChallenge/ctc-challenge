bc737fd46d717e1ceaf8b5c6d835b2642724e211
Kenny Koch <kennykoch47@gmail.com>
2018-11-13 00:21:08 -0500

    fixing heroku env bug

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path')
 
 
+
 // if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 require('dotenv').config();
 // Create the app
@@ -14,8 +15,6 @@ app.use(express.static(path.join(__dirname, './public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${  PORT}`);
