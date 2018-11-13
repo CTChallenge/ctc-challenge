e863e7cfd5e4ec1d6c480542a4d54ca5c7a4e1aa
Jason A. Young <youngj25@southernct.edu>
2018-11-13 17:15:00 -0500

    Add files via upload
    removed break statements...

--- client/components/mapContainer.js
+++ client/components/mapContainer.js
@@ -57,7 +57,7 @@ export class MapContainer extends Component {
      onMarkerClick(props, marker, e) {
 		 console.log(props);
 		 document.getElementById("markerTitle").innerHTML = props.title;
-		 document.getElementById("markerAddress").innerHTML = <b>Address:</b> <br> props.address <br> props.city;
+		 document.getElementById("markerAddress").innerHTML = <b>Address:</b> props.address + ", "+props.city;
 		 //document.getElementById("markerAddress").innerHTML = <b>Address:</b> <br> props.address <br> props.city + ", CT "+ props.zip.substring(0,5);
 		 document.getElementById("markerPhone").innerHTML = <b>Phone:</b> <a href="tel:"+props.phone>  props.phone </a> ;
 		 
