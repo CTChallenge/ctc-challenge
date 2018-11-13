1271a6200dc7dd3116c193784237dcfb9dcaddee
Kenny Koch <kennykoch47@gmail.com>
2018-11-12 21:16:40 -0500

    dotenv heroku bug fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
-require('dotenv').config();
+// require('dotenv').config();
 // Create the app
 const app = express();
 
