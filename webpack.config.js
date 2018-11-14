08789ac6e3c3911013b9276a158c4cce0a8c44ad
Kenny Koch <kennykoch47@gmail.com>
2018-11-14 10:54:41 -0500

    fixing heroku env bug

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"',
+        REACT_APP_GOOGLEKEY: process.env.REACT_APP_GOOGLEKEY,
       }
     })
   ],
